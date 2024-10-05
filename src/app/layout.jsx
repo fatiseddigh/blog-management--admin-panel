6a7e66d176912c284d5ad9248b91b5efe674a90e
fatiseddigh <seddighfati@gmail.com>
2024-10-05 21:43:30 +0400

    feat:add meta data

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,15 @@
 import Header from "@/components/Header";
 import afacadFont from "@/constants/afacadFont";
 import "@/styles/globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s |  Blog App",
+    default: " BLOG APP", // a default is required when creating a template
+  },
+  description: "Blog Management Web Application ",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
