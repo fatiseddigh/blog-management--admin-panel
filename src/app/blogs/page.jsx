62f357f73ca0cb2cf7730c85e82d22aec9ea60f1
fatiseddigh <seddighfati@gmail.com>
2024-10-06 13:50:34 +0400

    feat:add post list component+ fetch data

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,5 +1,12 @@
-function page() {
-  return <div>Bloggggg</div>;
+import PostList from "./_components/PostList";
+
+async function page() {
+  return (
+    <div>
+      <p className="text-secondary-500 mb-5"> this is blog</p>
+      <PostList />
+    </div>
+  );
 }
 
 export default page;
