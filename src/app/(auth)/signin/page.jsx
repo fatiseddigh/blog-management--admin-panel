a8eb3ef3f7845305c3db06f555ecafed5b44b454
fatiseddigh <seddighfati@gmail.com>
2024-10-13 20:10:13 +0400

    feat:add sign in page + api

---
+++ src/app/(auth)/signin/page.jsx
@@ -0,0 +1,70 @@
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import { singinApi } from "@/services/authService";
+
+const schema = yup
+  .object({
+    email: yup.string().email("Email is invalid").required("Email is required"),
+    password: yup.string().required("Password is required"),
+  })
+  .required();
+
+function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isLoading },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onTouched",
+  });
+
+  const onSubmit = async (values) => {
+    try {
+      const { user, message } = await singinApi(values);
+      console.log(user);
+      toast.success(message);
+      // router.push("/profile");
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-secondary-500 text-center mb-6">
+        Sign In
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+        <RHFTextField
+          label="email"
+          name="email"
+          register={register}
+          isRequired
+          errors={errors}
+        />
+        <RHFTextField
+          label="password"
+          name="password"
+          register={register}
+          type="password"
+          isRequired
+          errors={errors}
+        />
+
+        <Button type="submit" variant="primary" className="w-full">
+          Login
+        </Button>
+      </form>
+      <Link href="/signup" className="text-secondary-500 mt-6 text-center">
+        sign up
+      </Link>
+    </div>
+  );
+}
+export default SignIn;
